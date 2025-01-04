9a81ee412afee915ab2ea5ff2353d09ea954f737
Gaston Moure <gaston.moure@gmail.com>
2025-01-04 15:54:21 +0100

    .env file added

--- fast news/src/Components/NewsBoard.jsx
+++ fast news/src/Components/NewsBoard.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
 import NewsItem from './NewsItem'
 
-const API_KEY='54419fb992ba4f49ba47f70c63a98ab1'
+const apiKey = import.meta.env.VITE_API_KEY
+
+console.log(apiKey)
 
 const NewsBoard = ({ category }) => {
 
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`
   
   fetch(url).then(response => response.json()).then(data => setArticles(data.articles))
   }, [category])
